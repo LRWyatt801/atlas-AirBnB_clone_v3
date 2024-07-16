cd2a53fa9b7ec860cb138ccc0a14bbce336511f6
EliBrank <eliasbrinkman@gmail.com>
2024-07-16 06:38:12 +0000

    enable CORS for all routes

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,14 @@
 
 from os import getenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views, url_prefix="/api/v1")
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
